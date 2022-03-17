54253c644234b0e6599af918fe4e0e525b067487
Guido Ghezzi <ghezzizzo@gmail.com>
2022-03-18 00:23:01 +0100

    add routes login page, register page, reset page and dashboard page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
+import "./css/App.css";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Reset from "./pages/Reset";
+
+function App() {
+  return <div className="App">
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+
+  </div>;
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+  return (
+    <div>
+        <h1>Home page</h1>
+        <Link to="/login">Login</Link> now.
+        </div>
+  )
+}
+
+export default Home
